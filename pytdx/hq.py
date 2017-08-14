1e95dd454d525bf699aa94cf42b9462d79709ead
yutiansut <yutiansut@qq.com>
2017-08-15 00:42:20 +0800

    #commit 修复get_k_data

--- pytdx/hq.py
+++ pytdx/hq.py
@@ -11,7 +11,8 @@ import sys
 import pandas as pd
 
 if __name__ == '__main__':
-    sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    sys.path.append(os.path.dirname(
+        os.path.dirname(os.path.realpath(__file__))))
 
 from pytdx.log import DEBUG, log
 from pytdx.parser.get_security_bars import GetSecurityBarsCmd
@@ -27,12 +28,13 @@ from pytdx.parser.get_company_info_category import GetCompanyInfoCategory
 from pytdx.parser.get_company_info_content import GetCompanyInfoContent
 from pytdx.parser.get_xdxr_info import GetXdXrInfo
 from pytdx.parser.get_finance_info import GetFinanceInfo
-from pytdx.util import get_real_trade_date,trade_date_sse
+from pytdx.util import get_real_trade_date, trade_date_sse
 from pytdx.params import TDXParams
 from pytdx.heartbeat import HqHeartBeatThread
 
 from pytdx.parser.setup_commands import SetupCmd1, SetupCmd2, SetupCmd3
-import threading,datetime
+import threading
+import datetime
 import random
 
 from pytdx.base_socket_client import BaseSocketClient, update_last_ack_time
@@ -45,7 +47,7 @@ class TdxHq_API(BaseSocketClient):
         SetupCmd2(self.client).call_api()
         SetupCmd3(self.client).call_api()
 
-    #### API List
+    # API List
 
     @update_last_ack_time
     def get_security_bars(self, category, market, code, start, count):
@@ -128,25 +130,57 @@ class TdxHq_API(BaseSocketClient):
     def do_heartbeat(self):
         self.get_security_count(random.randint(0, 1))
 
-
-    def get_k_data(self, code, start,end):
+    def get_k_data(self, code, start_date, end_date):
         # 具体详情参见 https://github.com/rainx/pytdx/issues/5
-        if str(code)[0]=='6':
-            #0 - 深圳， 1 - 上海
-            market_code=1
+        if str(code)[0] == '6':
+            # 0 - 深圳， 1 - 上海
+            market_code = 1
         else:
-            market_code=0
-        start_date=get_real_trade_date(start,1)
-        end_date=get_real_trade_date(end,-1)
-        index_0=str(datetime.date.today())
-        index_of_index_0=trade_date_sse.index(index_0)
-        index_of_index_end=trade_date_sse.index(end_date)
-        index_of_index_start=trade_date_sse.index(start_date)
-        
-        index_of_end=index_of_index_0-index_of_index_end
-        index_length=index_of_index_end+1-index_of_index_start
-        return self.get_security_bars(9, market_code, code,index_of_end, index_length)  # 返回普通list
-        
+            market_code = 0
+        if str(code)[0] == '6':
+            # 0 - 深圳， 1 - 上海
+            market_code = 1
+        else:
+            market_code = 0
+
+        start_date = get_real_trade_date(start_date, 1)
+        end_date = get_real_trade_date(end_date, -1)
+
+
+        # 判断end_date在哪个位置
+        index_0 = str(datetime.date.today())
+        index_of_index_0 = trade_date_sse.index(index_0)
+        index_of_index_end = trade_date_sse.index(
+            end_date)  # 这是我们想要的date 在那个trade_date里的位置
+        index_of_index_start = trade_date_sse.index(start_date)
+
+        index_of_end = index_of_index_0 - index_of_index_end
+        #data = api.to_df(api.get_security_bars(9, market_code, code,index_of_end, 1))
+        # 这是拿到的真实的date
+        date = str(list(self.to_df(self.get_security_bars(
+            9, market_code, code, index_of_end, 1))['datetime'])[0])[0:10]
+
+        def judge_date(date, index_of_index_end, index_of_end):
+            while abs(index_of_index_end - trade_date_sse.index(date)) != 0:
+                index_of_end -= index_of_index_end - \
+                    trade_date_sse.index(date)  # 这个是实际拿到的时间的位置
+                date = str(list(self.to_df(self.get_security_bars(
+                    9, market_code, code, index_of_end, 1))['datetime'])[0])[0:10]
+                return index_of_end
+
+        index_of_end = judge_date(date, index_of_index_end, index_of_end)
+        index_length = index_of_index_end + 1 - index_of_index_start + 20
+
+        # data = api.get_security_bars(9, market_code, code,index_of_end, index_length)  # 返回普通list
+        data = self.to_df(self.get_security_bars(
+            9, market_code, code, index_of_end, index_length))  # 返回DataFrame
+        data['date'] = data['datetime'].apply(lambda x: x[0:10])
+        data = data.set_index('date')
+        data = data.drop(['year', 'month', 'day', 'hour',
+                            'minute', 'datetime'], axis=1)
+
+        return data[start_date:end_date]
+
 
 if __name__ == '__main__':
     import pprint
@@ -157,7 +191,7 @@ if __name__ == '__main__':
         stocks = api.get_security_quotes([(0, "000001"), (1, "600300")])
         pprint.pprint(stocks)
         log.info("获取k线")
-        data = api.get_security_bars(9,0, '000001', 4, 3)
+        data = api.get_security_bars(9, 0, '000001', 4, 3)
         pprint.pprint(data)
         log.info("获取 深市 股票数量")
         pprint.pprint(api.get_security_count(0))
@@ -165,19 +199,21 @@ if __name__ == '__main__':
         stocks = api.get_security_list(1, 255)
         pprint.pprint(stocks)
         log.info("获取指数k线")
-        data = api.get_index_bars(9,1, '000001', 1, 2)
+        data = api.get_index_bars(9, 1, '000001', 1, 2)
         pprint.pprint(data)
         log.info("查询分时行情")
         data = api.get_minute_time_data(TDXParams.MARKET_SH, '600300')
         pprint.pprint(data)
         log.info("查询历史分时行情")
-        data = api.get_history_minute_time_data(TDXParams.MARKET_SH, '600300', 20161209)
+        data = api.get_history_minute_time_data(
+            TDXParams.MARKET_SH, '600300', 20161209)
         pprint.pprint(data)
         log.info("查询分时成交")
         data = api.get_transaction_data(TDXParams.MARKET_SZ, '000001', 0, 30)
         pprint.pprint(data)
         log.info("查询历史分时成交")
-        data = api.get_history_transaction_data(TDXParams.MARKET_SZ, '000001', 0, 10, 20170209)
+        data = api.get_history_transaction_data(
+            TDXParams.MARKET_SZ, '000001', 0, 10, 20170209)
         pprint.pprint(data)
         log.info("查询公司信息目录")
         data = api.get_company_info_category(TDXParams.MARKET_SZ, '000001')
@@ -192,10 +228,7 @@ if __name__ == '__main__':
         data = api.get_finance_info(0, '000001')
         pprint.pprint(data)
         log.info("日线级别k线获取函数")
-        data =api.get_k_data('000001','2017-07-01','2017-07-10')
+        data = api.get_k_data('000001', '2017-07-01', '2017-07-10')
         pprint.pprint(data)
 
         api.disconnect()
-
-
-
