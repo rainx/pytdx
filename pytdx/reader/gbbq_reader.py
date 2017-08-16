f313fadbae7fd2e7bcd2a57ee3e676fb6e1b499b
RainX <i@rainx.cc>
2017-08-16 12:53:37 +0800

    fix stupid int and unsigned int issue

--- pytdx/reader/gbbq_reader.py
+++ pytdx/reader/gbbq_reader.py
@@ -2,6 +2,8 @@
 
 import struct
 from ctypes import *
+import pandas as pd
+import sys
 
 
 ### take ref this article :http://blog.csdn.net/fangle6688/article/details/50956609
@@ -10,8 +12,11 @@ class GbbqReader(object):
 
 
     def get_def(self, fname):
-        bin_keys = bytes.fromhex(self.hexdump_keys)
-
+        if sys.version_info.major == 2:
+            bin_keys = bytearray.fromhex(self.hexdump_keys)
+        else:
+            bin_keys = bytes.fromhex(self.hexdump_keys)
+        result = []
         with open(fname, "rb") as f:
             content = f.read()
             pos = 0
@@ -20,64 +25,64 @@ class GbbqReader(object):
             encrypt_data = content
             # data_len = len(encrypt_data)
             data_offset = pos
+
             for _ in range(count):
                 clear_data = bytearray()
                 for i in range(3):
-                    (eax, ) = struct.unpack("<i", bin_keys[0x44: 0x44 + 4])
-                    (ebx, ) = struct.unpack("<i", encrypt_data[data_offset: data_offset+4])
-                    num = c_int32(eax ^ ebx).value
-                    (numold, ) = struct.unpack("<i", encrypt_data[data_offset + 0x4: data_offset + 0x4 + 4])
+                    (eax, ) = struct.unpack("<I", bin_keys[0x44: 0x44 + 4])
+                    (ebx, ) = struct.unpack("<I", encrypt_data[data_offset: data_offset+4])
+                    num = c_uint32(eax ^ ebx).value
+                    (numold, ) = struct.unpack("<I", encrypt_data[data_offset + 0x4: data_offset + 0x4 + 4])
                     for j in reversed(range(4, 0x40+4, 4)):
                         ebx = (num & 0xff0000) >> 16
-                        (eax, ) = struct.unpack("<i", bin_keys[ebx * 4 + 0x448: ebx * 4 + 0x448 + 4])
+                        (eax, ) = struct.unpack("<I", bin_keys[ebx * 4 + 0x448: ebx * 4 + 0x448 + 4])
                         ebx = num >> 24
-                        (eax_add, ) = struct.unpack("<i", bin_keys[ebx * 4 + 0x48: ebx * 4 + 0x48 + 4])
+                        (eax_add, ) = struct.unpack("<I", bin_keys[ebx * 4 + 0x48: ebx * 4 + 0x48 + 4])
                         eax += eax_add
-                        eax = c_int32(eax).value
+                        eax = c_uint32(eax).value
                         ebx = (num & 0xff00) >> 8
-                        (eax_xor, ) = struct.unpack("<i", bin_keys[ebx * 4 + 0x848: ebx * 4 + 0x848 + 4])
+                        (eax_xor, ) = struct.unpack("<I", bin_keys[ebx * 4 + 0x848: ebx * 4 + 0x848 + 4])
                         eax ^= eax_xor
-                        eax = c_int32(eax).value
+                        eax = c_uint32(eax).value
                         ebx = num & 0xff
-                        (eax_add, ) = struct.unpack("<i", bin_keys[ebx * 4 + 0xC48: ebx * 4 + 0xC48 + 4])
+                        (eax_add, ) = struct.unpack("<I", bin_keys[ebx * 4 + 0xC48: ebx * 4 + 0xC48 + 4])
                         eax += eax_add
-                        eax = c_int32(eax).value
-                        (eax_xor, ) = struct.unpack("<i", bin_keys[j: j + 4])
+                        eax = c_uint32(eax).value
+                        (eax_xor, ) = struct.unpack("<I", bin_keys[j: j + 4])
                         eax ^= eax_xor
-                        eax = c_int32(eax).value
-
+                        eax = c_uint32(eax).value
                         ebx = num
                         num = numold ^ eax
-                        num = c_int32(num).value
+                        num = c_uint32(num).value
                         numold = ebx
 
-
-                    (numold_op, ) = struct.unpack("<i", bin_keys[0: 4])
+                    (numold_op, ) = struct.unpack("<I", bin_keys[0: 4])
                     numold ^= numold_op
-                    numold = c_int32(numold).value
-                    print(numold, num)
-                    clear_data.extend(struct.pack("<ii", numold, num))
+                    numold = c_uint32(numold).value
+                    clear_data.extend(struct.pack("<II", numold, num))
                     data_offset += 8
 
                 clear_data.extend(encrypt_data[data_offset: data_offset+5])
 
-                print(clear_data)
-
-                print(struct.unpack("<8sIBffff", clear_data))
-
-                if data_offset > 500:
-                    break
-
+                (v1,v2, v3,v4,v5,v6,v7,v8) = (struct.unpack("<B7sIBffff", clear_data))
+                line = (v1,
+                        v2.rstrip(b"\x00").decode("utf-8"),
+                        v3,
+                        v4,
+                        v5,
+                        v6,
+                        v7,
+                        v8)
+                result.append(line)
                 data_offset += 5
 
 
-
-
-
-
-
-
-
+        df = pd.DataFrame(data=result, columns=['market', 'code', 'datetime', 'category',
+                                                  'hongli_panqianliutong',
+                                                  'peigujia_qianzongguben',
+                                                  'songgu_qianzongguben',
+                                                  'peigu_houzongguben'])
+        return df
 
 
 
@@ -87,3 +92,4 @@ class GbbqReader(object):
 
 if __name__ == '__main__':
     result = GbbqReader().get_def("/Users/rainx/tmp/gbbq")
+    print(result)
