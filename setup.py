05c72767cf9e8bb723c35f3f103e67c5cab7e2ae
yutiansut <yutiansut@qq.com>
2017-08-21 12:13:53 +0800

    # version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except (IOError, ImportError):
 
 setup(
     name='pytdx',
-    version='1.31',
+    version='1.32',
     description='A Python Interface to TDX protocol',
     long_description=long_description,
     author='RainX<Jing Xu>',
