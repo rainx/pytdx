732128f5d2cce3c237a6ef8f776c09ebe3ffed2d
RainX <i@rainx.cc>
2018-02-13 17:11:45 +0800

    读取专业财务数据

--- pytdx/crawler/history_financial_crawler.py
+++ pytdx/crawler/history_financial_crawler.py
@@ -6,6 +6,18 @@ import shutil
 import tempfile
 import random
 import os
+import six
+import pandas as pd
+
+if six.PY2:
+    import zipfile
+
+"""
+https://github.com/rainx/pytdx/issues/133
+
+获取历史财务数据的接口，参考上面issue里面 @datochan 的方案和代码
+
+"""
 
 class HistoryFinancialListCrawler(BaseCralwer):
 
@@ -38,12 +50,18 @@ class HistoryFinancialCrawler(BaseCralwer):
 
     def parse(self, download_file, *args, **kwargs):
 
+        header_pack_format = '<1hI1H3L'
+
         tmpdir_root = tempfile.gettempdir()
         subdir_name = "pytdx_" + str(random.randint(0, 1000000))
         tmpdir = os.path.join(tmpdir_root, subdir_name)
         shutil.rmtree(tmpdir, ignore_errors=True)
-        os.makedirs(tmpdir, exist_ok=True)
-        shutil.unpack_archive(download_file.name, extract_dir=tmpdir)
+        os.makedirs(tmpdir)
+        if six.PY2:
+            with zipfile.ZipFile(download_file.name, 'r') as zf:
+                zf.extractall(tmpdir)
+        else:
+            shutil.unpack_archive(download_file.name, extract_dir=tmpdir)
         # only one file endswith .dat should be in zip archives
         datfile = None
         for _file in os.listdir(tmpdir):
@@ -53,30 +71,50 @@ class HistoryFinancialCrawler(BaseCralwer):
         if datfile is None:
             raise Exception("no dat file found in zip archive")
 
-        header_size = calcsize("<3h1H3L")
+        header_size = calcsize(header_pack_format)
         stock_item_size = calcsize("<6s1c1L")
         data_header = datfile.read(header_size)
-        stock_header = unpack("<3h1H3L", data_header)
-        max_count = stock_header[3]
+        stock_header = unpack(header_pack_format, data_header)
+        print(stock_header)
+        max_count = stock_header[2]
+        report_date = stock_header[1]
+        report_size = stock_header[4]
+        report_fields_count = int(report_size / 4)
+        report_pack_format = '<{}f'.format(report_fields_count)
 
+        results = []
         for stock_idx in range(0, max_count):
             datfile.seek(header_size + stock_idx * calcsize("<6s1c1L"))
             si = datfile.read(stock_item_size)
             stock_item = unpack("<6s1c1L", si)
             code = stock_item[0].decode("utf-8")
             foa = stock_item[2]
-            print(code, foa)
             datfile.seek(foa)
-            info_data = datfile.read(calcsize('<264f'))
-            data_size = len(info_data)
-            cw_info = unpack('<264f', info_data)
-            print("%s, %s" % (code, str(cw_info)))
+
+            info_data = datfile.read(calcsize(report_pack_format))
+            cw_info = unpack(report_pack_format, info_data)
+            one_record = (code, report_date) + cw_info
+            results.append(one_record)
 
         datfile.close()
-
         shutil.rmtree(tmpdir, ignore_errors=True)
+        return results
+
+    def to_df(self, data):
+        if len(data) == 0:
+            return None
+
+        total_lengh = len(data[0])
+        col = ['code', 'report_date']
+
+        length = total_lengh - 2
+        for i in range(0, length):
+            col.append("col" + str(i + 1))
 
 
+        df =  pd.DataFrame(data=data, columns=col)
+        df.set_index('code', inplace=True)
+        return df
 
 
 if __name__ == '__main__':
@@ -92,7 +130,9 @@ if __name__ == '__main__':
     # filename = list_data[1]['filename']
     #
     datacrawler = HistoryFinancialCrawler()
+    pd.set_option('display.max_columns', None)
+
     # result = datacrawler.fetch_and_parse(reporthook=demo_reporthook, filename=filename, path_to_download="/tmp/tmpfile.zip")
     with open(r"/tmp/tmpfile.zip", "rb") as fp:
         result = datacrawler.parse(download_file=fp)
-        print(result)
+        print(datacrawler.to_df(data=result))
